91080984b8a9c6cf942985d371adb172d988ef0b
kumar ishan <kumarishan1116@gmail.com>
2025-11-23 23:55:21 +0530

    Updates

---
+++ React-final.6bd02f5a.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, React.createElement("h1", {}, "Ishan Here!")));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=React-final.6bd02f5a.js.map
